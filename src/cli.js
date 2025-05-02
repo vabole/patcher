41d1355cdf18dfc5284df96b64a392beba72a63a
Ilia Safronov <ilia.safronov@ah.nl>
2025-05-02 12:46:23 +0200

    Update version to 1.0.0

--- src/cli.js
+++ src/cli.js
@@ -14,7 +14,7 @@ const __dirname = path.dirname(fileURLToPath(import.meta.url));
 program
   .name('patcher')
   .description('Patch installed npm packages')
-  .version('0.3.1')
+  .version('1.0.0')
   .argument('<package-or-config>', 'Package name or path to configuration file')
   .option('-u, --undo', 'Undo previous patches')
   .option('-c, --create', 'Create a default configuration file for the package in ~/.patcher')
